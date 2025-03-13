65fe06de22bc32067f7347c6279c5a2f23b2faaf
LittleSheep <littlesheep.code@hotmail.com>
2025-03-13 22:26:35 +0800

    :recycle: Optimized fediverse post displaying

--- lib/widgets/post/fediverse_post_item.dart
+++ lib/widgets/post/fediverse_post_item.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:html2md/html2md.dart' as html2md;
+import 'package:relative_time/relative_time.dart';
 import 'package:styled_widget/styled_widget.dart';
 import 'package:surface/types/post.dart';
-import 'package:html/parser.dart';
 import 'package:surface/widgets/account/account_image.dart';
 import 'package:surface/widgets/attachment/attachment_list.dart';
-import 'package:surface/widgets/html.dart';
+import 'package:surface/widgets/markdown_content.dart';
 import 'package:surface/widgets/universal_image.dart';
 
 class FediversePostWidget extends StatelessWidget {
   final SnFediversePost data;
   final double maxWidth;
-  const FediversePostWidget(
-      {super.key, required this.data, required this.maxWidth});
+  const FediversePostWidget({
+    super.key,
+    required this.data,
+    required this.maxWidth,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final borderSide =
-        BorderSide(width: 1, color: Theme.of(context).dividerColor);
-    final backgroundColor = Theme.of(context).colorScheme.surfaceContainer;
-
     return Center(
       child: Container(
         constraints: BoxConstraints(maxWidth: maxWidth),
@@ -30,8 +30,11 @@ class FediversePostWidget extends StatelessWidget {
             children: [
               Row(
                 children: [
-                  AccountImage(content: data.user.avatar),
-                  const Gap(8),
+                  AccountImage(
+                    content: data.user.avatar,
+                    radius: 20,
+                  ),
+                  const Gap(12),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -40,66 +43,125 @@ class FediversePostWidget extends StatelessWidget {
                             ? data.user.nick
                             : '@${data.user.name}',
                       ).bold(),
-                      Text(data.user.identifier),
+                      Row(
+                        children: [
+                          Text(
+                            data.user.identifier.contains('@')
+                                ? data.user.identifier
+                                : '${data.user.identifier}@${data.user.origin}',
+                          ).fontSize(13),
+                          const Gap(4),
+                          Text(
+                            RelativeTime(context)
+                                .format(data.createdAt.toLocal()),
+                          ).fontSize(13),
+                        ],
+                      ),
                     ],
                   ),
                 ],
-              ),
-              const Gap(8),
-              ...parseHtmlToWidgets(context, parse(data.content).children),
+              ).padding(horizontal: 12, vertical: 8),
+              MarkdownTextContent(
+                isAutoWarp: true,
+                content: html2md.convert(data.content),
+              ).padding(horizontal: 16, bottom: 6),
               if (data.images.isNotEmpty)
-                AspectRatio(
-                  aspectRatio: 1,
-                  child: ScrollConfiguration(
-                    behavior: AttachmentListScrollBehavior(),
-                    child: ListView.separated(
-                      shrinkWrap: true,
-                      itemCount: data.images.length,
-                      itemBuilder: (context, idx) {
-                        return Container(
-                          constraints: BoxConstraints(maxWidth: maxWidth),
-                          child: AspectRatio(
-                            aspectRatio: 1,
-                            child: Stack(
-                              fit: StackFit.expand,
-                              children: [
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: backgroundColor,
-                                    border: Border(
-                                      top: borderSide,
-                                      bottom: borderSide,
-                                    ),
-                                    borderRadius: AttachmentList.kDefaultRadius,
-                                  ),
-                                  child: ClipRRect(
-                                    borderRadius: AttachmentList.kDefaultRadius,
-                                    child: AutoResizeUniversalImage(
-                                      data.images[idx],
-                                    ),
-                                  ),
-                                ),
-                                Positioned(
-                                  right: 8,
-                                  bottom: 8,
-                                  child: Chip(
-                                    label: Text(
-                                        '${idx + 1}/${data.images.length}'),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                      separatorBuilder: (context, index) => const Gap(8),
-                      physics: const BouncingScrollPhysics(),
-                      scrollDirection: Axis.horizontal,
-                    ),
-                  ),
+                _FediversePostImageList(
+                  data: data,
+                  maxWidth: maxWidth,
                 ),
             ],
-          ).padding(all: 8),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _FediversePostImageList extends StatelessWidget {
+  const _FediversePostImageList({
+    required this.data,
+    required this.maxWidth,
+  });
+
+  final SnFediversePost data;
+  final double maxWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    final borderSide =
+        BorderSide(width: 1, color: Theme.of(context).dividerColor);
+    final backgroundColor = Theme.of(context).colorScheme.surfaceContainer;
+
+    if (data.images.length == 1) {
+      return AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          constraints: BoxConstraints(maxWidth: maxWidth),
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            border: Border(
+              top: borderSide,
+              bottom: borderSide,
+            ),
+            borderRadius: AttachmentList.kDefaultRadius,
+          ),
+          child: ClipRRect(
+            borderRadius: AttachmentList.kDefaultRadius,
+            child: AutoResizeUniversalImage(
+              data.images.first,
+            ),
+          ),
+        ),
+      ).padding(horizontal: 8);
+    }
+
+    return AspectRatio(
+      aspectRatio: 1,
+      child: ScrollConfiguration(
+        behavior: AttachmentListScrollBehavior(),
+        child: ListView.separated(
+          shrinkWrap: true,
+          itemCount: data.images.length,
+          itemBuilder: (context, idx) {
+            return Container(
+              constraints: BoxConstraints(maxWidth: maxWidth),
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: backgroundColor,
+                        border: Border(
+                          top: borderSide,
+                          bottom: borderSide,
+                        ),
+                        borderRadius: AttachmentList.kDefaultRadius,
+                      ),
+                      child: ClipRRect(
+                        borderRadius: AttachmentList.kDefaultRadius,
+                        child: AutoResizeUniversalImage(
+                          data.images[idx],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 8,
+                      bottom: 8,
+                      child: Chip(
+                        label: Text('${idx + 1}/${data.images.length}'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => const Gap(8),
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
         ),
       ),
     );
